82153e22180834e83680fdd67f72904c33dddfbe
Anna Bobrova <ann.bobrovq@mail.ru>
2019-03-28 01:35:19 +0300

    Translate

--- WebHookBot.py
+++ WebHookBot.py
@@ -6,8 +6,12 @@ server = Flask(__name__)
 
 @server.route("/bot", methods=['POST'])
 def getMessage():
-    text = request.get_json()
-    print(text['message']['text'])
+    text = request.get_json()['message']['text']
+    url = f'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190326T205614Z.90fe981868ebe21d.07ae7a57abb1ff0f1f3912f6c5af593880090026&text={text}&lang=en'
+    translated_text = requests.get(url).json()['text'][0]
+    print(translated_text)
+    #f'{text} -> {translated_text}'
+
     return 'ok'
 
 def main():
