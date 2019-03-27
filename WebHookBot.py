7b713c833d8c906887e520874476de1bbeaf903b
Anna Bobrova <ann.bobrovq@mail.ru>
2019-03-28 02:35:36 +0300

    Final fix

--- WebHookBot.py
+++ WebHookBot.py
@@ -9,13 +9,12 @@ yandex_key = os.environ['YANDEX_KEY']
 @server.route("/bot", methods=['POST'])
 def getMessage():
     message = request.get_json()['message']
-    text = message['text']
-    url = f'https://translate.yandex.net/api/v1.5/tr.json/translate?key={yandex_key}&text={text}&lang=en'
+    text_to_translate = message['text']
+    url = f'https://translate.yandex.net/api/v1.5/tr.json/translate?key={yandex_key}&text={text_to_translate}&lang=en'
     translated_text = requests.get(url).json()['text'][0]
 
-    params = {'chat_id': message['chat']['id'], 'text': f'{text} -> {translated_text}'}
-    method = 'sendMessage'
-    requests.post(f'https://api.telegram.org/bot{bot_token}/{method}', params)
+    body_data = {'chat_id': message['chat']['id'], 'text': f'{text_to_translate} -> {translated_text}'}
+    requests.post(f'https://api.telegram.org/bot{bot_token}/sendMessage', body_data)
 
     return 'ok'
 
