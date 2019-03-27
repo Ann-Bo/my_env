83f759eaa44dc4667ea01055f868f1409edc84a5
Anna Bobrova <ann.bobrovq@mail.ru>
2019-03-27 23:47:06 +0300

    Language fix

--- Translator_Bot.py
+++ Translator_Bot.py
@@ -24,7 +24,7 @@ class BotHandler:
         return get_result[-1] if len(get_result) > 0 else None
 
     def translate(self, text):
-        url = f'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190326T205614Z.90fe981868ebe21d.07ae7a57abb1ff0f1f3912f6c5af593880090026&text={text}&lang=ru-en'
+        url = f'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190326T205614Z.90fe981868ebe21d.07ae7a57abb1ff0f1f3912f6c5af593880090026&text={text}&lang=en'
         translated_text = requests.get(url).json()['text'][0]
         return f'{text} -> {translated_text}'
 
