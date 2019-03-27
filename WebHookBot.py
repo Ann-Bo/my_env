ed602a85aeb48949005b09eb65fc76249786f207
Anna Bobrova <ann.bobrovq@mail.ru>
2019-03-28 02:12:37 +0300

    ENV

--- WebHookBot.py
+++ WebHookBot.py
@@ -3,17 +3,19 @@ import os
 from flask import Flask, request
 
 server = Flask(__name__)
+bot_token = os.environ['BOT_TOKEN']
+yandex_key = os.environ['YANDEX_KEY']
 
 @server.route("/bot", methods=['POST'])
 def getMessage():
     message = request.get_json()['message']
     text = message['text']
-    url = f'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190326T205614Z.90fe981868ebe21d.07ae7a57abb1ff0f1f3912f6c5af593880090026&text={text}&lang=en'
+    url = f'https://translate.yandex.net/api/v1.5/tr.json/translate?key={yandex_key}&text={text}&lang=en'
     translated_text = requests.get(url).json()['text'][0]
 
     params = {'chat_id': message['chat']['id'], 'text': f'{text} -> {translated_text}'}
     method = 'sendMessage'
-    resp = requests.post('https://api.telegram.org/bot882179046:AAGLbbPeIlEhHWI8oVKfrbSWpDWQ2yVfjy0/' + method, params)
+    requests.post(f'https://api.telegram.org/{bot_token}/{method}', params)
 
     return 'ok'
 
